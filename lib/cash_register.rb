c72eea22e60f1251bafbf86677a3457a27dd416b
Danny Rivera <samboard@hotmail.com>
2021-02-12 12:12:46 -0500

    commit tree 6b5b84b7ca2299a3c77730c6dff3f6665ce98da2

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
+require 'pry'
+class CashRegister
+    attr_reader :discount, :items
+    attr_accessor :total
 
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def add_item(item, price, quantity=1)
+        quantity.times do
+            @items << item
+        end
+        @last_transaction = price * quantity
+        @total += @last_transaction
+    end
+
+    def apply_discount
+        if @discount > 0
+            @total = @total -(@total * discount / 100)
+            return "After the discount, the total comes to $#{@total}."
+        else
+            return "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+        @total -= @last_transaction
+        #@quantity.times do
+            #@items.pop()
+        #end
+    end
+end
+
+#binding.pry
